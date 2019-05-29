1293a38ddbca8b524256853d387a208f7ca35000
Arli Machado <arli.machado@b2wdigital.com>
2019-05-29 17:38:05 -0300

    Configurando actions e store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import './config/DevToolsConfig';
 import './config/ReactotronConfig';
+import React from 'react';
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style>Welcome to React Native!</Text>
-      </View>
-    );
-  }
-}
+import store from './store';
+import TodoList from './TodoList';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
+const App = () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+
+export default App;
